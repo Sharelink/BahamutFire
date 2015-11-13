a16d763b3bbcf91445b9daeae68603b634be28fb
AlexChow <cplover@live.com>
2015-11-13 14:50:04 +0800

    rtm

--- src/APIServer/Startup.cs
+++ src/APIServer/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Dnx.Runtime;
 using ServiceStack.Redis;
 using ServerControlService.Model;
 using NLog;
+using ServerControlService.Service;
 
 namespace BahamutFire.APIServer
 {
@@ -40,6 +41,7 @@ namespace BahamutFire.APIServer
             Appkey = Configuration["Data:App:appkey"];
         }
         public static TokenService TokenService { private set; get; }
+        public static ServerControlManagementService ServerControlMgrService { get; set; }
         public static string BahamutFireDbUrl { get; private set; }
         public static string Appkey { get; private set; }
         public static string AppUrl { get; private set; }
@@ -55,14 +57,24 @@ namespace BahamutFire.APIServer
 
             var serverControlUrl = Configuration["Data:ControlServiceServer:url"].Replace("redis://", "");
             IRedisClientsManager ControlServerServiceClientManager = new PooledRedisClientManager(serverControlUrl);
-            var serverMgrService = new ServerControlService.Service.ServerControlManagementService(ControlServerServiceClientManager);
+            ServerControlMgrService = new ServerControlManagementService(ControlServerServiceClientManager);
             var appInstance = new BahamutAppInstance()
             {
                 Appkey = Appkey,
                 InstanceServiceUrl = Configuration["Data:App:url"]
             };
-            appInstance = serverMgrService.RegistAppInstance(appInstance);
-            serverMgrService.StartKeepAlive(appInstance.Id);
+            try
+            {
+                appInstance = ServerControlMgrService.RegistAppInstance(appInstance);
+                var observer = ServerControlMgrService.StartKeepAlive(appInstance);
+                observer.OnExpireError += KeepAliveObserver_OnExpireError;
+                observer.OnExpireOnce += KeepAliveObserver_OnExpireOnce;
+                LogManager.GetLogger("KeepAlive").Info("Keep Server Instance Alive To Server Controller Thread Started!");
+            }
+            catch (Exception ex)
+            {
+                LogManager.GetCurrentClassLogger().Error(ex, "Unable To Regist App Instance");
+            }
 
             // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
             // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
@@ -77,19 +89,18 @@ namespace BahamutFire.APIServer
             var fileTarget = new NLog.Targets.FileTarget();
             fileTarget.FileName = Configuration["Data:Log:logFile"];
             fileTarget.Name = "FileLogger";
-            fileTarget.Layout = @"${date:format=HH\:mm\:ss} ${logger}:${message}";
+            fileTarget.Layout = @"${date:format=HH\:mm\:ss} ${logger}:${message};${exception}";
             logConfig.AddTarget(fileTarget);
             logConfig.LoggingRules.Add(new NLog.Config.LoggingRule("*", NLog.LogLevel.Debug, fileTarget));
-            LogManager.Configuration = logConfig;
-
             if (env.IsDevelopment())
             {
                 var consoleLogger = new NLog.Targets.ColoredConsoleTarget();
                 consoleLogger.Name = "ConsoleLogger";
-                consoleLogger.Layout = @"${date:format=HH\:mm\:ss} ${logger}:${message}";
+                consoleLogger.Layout = @"${date:format=HH\:mm\:ss} ${logger}:${message};${exception}";
                 logConfig.AddTarget(consoleLogger);
                 logConfig.LoggingRules.Add(new NLog.Config.LoggingRule("*", NLog.LogLevel.Debug, consoleLogger));
             }
+            LogManager.Configuration = logConfig;
 
             // Configure the HTTP request pipeline.
             app.UseStaticFiles();
@@ -100,6 +111,17 @@ namespace BahamutFire.APIServer
             // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
 
             LogManager.GetCurrentClassLogger().Info("Toronto Started!");
+        }
+
+        private void KeepAliveObserver_OnExpireOnce(object sender, KeepAliveObserverEventArgs e)
+        {
+            LogManager.GetLogger("KeepAlive").Info(string.Format("Expired Instance:{0}", e.Instance.Id));
+        }
+
+        private void KeepAliveObserver_OnExpireError(object sender, KeepAliveObserverEventArgs e)
+        {
+            LogManager.GetLogger("KeepAlive").Error(string.Format("Expire Server Error.Instance:{0}", e.Instance.Id), e);
+            ServerControlMgrService.ReActiveAppInstance(e.Instance);
         }
     }
 }
