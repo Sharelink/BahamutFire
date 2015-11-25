76d1a0c3d5ce3d181eb5091187146b255814f370
AlexChow <cplover@live.com>
2015-11-25 13:56:35 +0800

    update 1.2

--- src/FireServer/Startup.cs
+++ src/FireServer/Startup.cs
@@ -15,7 +15,7 @@ namespace FireServer
 {
     public class Startup
     {
-        private readonly IConfiguration Configuration;
+        public static IConfiguration Configuration { private set; get; }
         public static TokenService TokenService { private set; get; }
         public static ServerControlManagementService ServerControlMgrService { get; set; }
         public static string BahamutFireDbUrl { get; private set; }
