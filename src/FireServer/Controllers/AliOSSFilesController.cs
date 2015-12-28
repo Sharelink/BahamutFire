0173ed074bab71a2945bdfebe428dafd4825aa19
AlexChow <cplover@live.com>
2015-12-28 20:08:28 +0800

    add post file list

--- src/FireServer/Controllers/AliOSSFilesController.cs
+++ src/FireServer/Controllers/AliOSSFilesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.Mvc;
 using BahamutFireService.Service;
 using BahamutFireCommon;
 using BahamutCommon;
+using System.Net;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,42 +21,89 @@ namespace FireServer.Controllers
     [Route("[controller]")]
     public class AliOSSFilesController : Controller
     {
+        private object FireRecordToObject(FireRecord newFire,string bucket)
+        {
+            var fireObj = new
+            {
+                fileId = newFire.Id.ToString(),
+                server = newFire.UploadServerUrl,
+                accessKey = newFire.Id.ToString(),
+                bucket = bucket,
+                serverType = AliOSSFileInfo.AliOssServerType,
+                expireAt = DateTimeUtil.ToString(DateTime.UtcNow.AddDays(7))
+            };
+            return fireObj;
+        }
+
+        private FireRecord GenerateNewFireRecord(string fileType,int fileSize,string accountId,string bucket)
+        {
+            var aliOssInfo = new AliOSSFileInfo()
+            {
+                Bucket = bucket
+            };
+            var newFire = new FireRecord()
+            {
+                CreateTime = DateTime.UtcNow,
+                FileSize = fileSize,
+                FileType = fileType,
+                IsSmallFile = fileSize < 1024 * 256,
+                State = (int)FireRecordState.Create,
+                AccountId = accountId,
+                AccessKeyConverter = "",
+                UploadServerUrl = Startup.Configuration["Data:AliOSS:url"],
+                ServerType = AliOSSFileInfo.AliOssServerType,
+                Extra = Newtonsoft.Json.JsonConvert.SerializeObject(aliOssInfo)
+            };
+            return newFire;
+        }
+
+        [HttpPost("List")]
+        public async Task<object> PostFileList(string fileTypes, string fileSizes)
+        {
+            try
+            {
+                var bucket = Startup.Configuration["Data:AliOSS:bucket"];
+                var fireService = new FireService(Startup.BahamutFireDbUrl);
+                var accountId = Request.Headers["accountId"];
+
+                var fileTypeList = fileTypes.Split('#');
+                var fileSizeList = fileSizes.Split('#');
+                if (fileTypeList.Count() != fileSizeList.Count())
+                {
+                    throw new Exception("New Fire Paramenters Not Match");
+                }
+                var newFires = new List<FireRecord>();
+                for (int i = 0; i < fileTypeList.Count(); i++)
+                {
+                    var fire = GenerateNewFireRecord(fileTypeList[i], int.Parse(fileSizeList[i]), accountId, bucket);
+                    newFires.Add(fire);
+                }
+                var records = await fireService.CreateFireRecords(newFires);
+                var result = new
+                {
+                    files = from r in records select FireRecordToObject(r, bucket)
+                };
+                return result;
+            }
+            catch (Exception ex)
+            {
+                NLog.LogManager.GetLogger("Warn").Warn(ex, "Add New Fires Error:{0}", ex.Message);
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return "Bad Request";
+            }
+        }
 
         [HttpPost]
         public async Task<object> Post(string fileType, int fileSize)
         {
             try
             {
+                var bucket = Startup.Configuration["Data:AliOSS:bucket"];
                 var fireService = new FireService(Startup.BahamutFireDbUrl);
                 var accountId = Request.Headers["accountId"];
-                var aliOssInfo = new AliOSSFileInfo()
-                {
-                    Bucket = Startup.Configuration["Data:AliOSS:bucket"]
-                };
-                var newFire = new FireRecord()
-                {
-                    CreateTime = DateTime.UtcNow,
-                    FileSize = fileSize,
-                    FileType = fileType,
-                    IsSmallFile = fileSize < 1024 * 256,
-                    State = (int)FireRecordState.Create,
-                    AccountId = accountId,
-                    AccessKeyConverter = "",
-                    UploadServerUrl = Startup.Configuration["Data:AliOSS:url"],
-                    ServerType = AliOSSFileInfo.AliOssServerType,
-                    Extra = Newtonsoft.Json.JsonConvert.SerializeObject(aliOssInfo)
-                };
-
+                var newFire = GenerateNewFireRecord(fileType, fileSize, accountId, bucket);
                 newFire = await fireService.CreateFireRecord(newFire);
-                return new
-                {
-                    fileId = newFire.Id.ToString(),
-                    server = newFire.UploadServerUrl,
-                    accessKey = newFire.Id.ToString(),
-                    bucket = aliOssInfo.Bucket,
-                    serverType = AliOSSFileInfo.AliOssServerType,
-                    expireAt = DateTimeUtil.ToString(DateTime.UtcNow.AddDays(7))
-                };
+                return FireRecordToObject(newFire, bucket);
             }
             catch (Exception ex)
             {
